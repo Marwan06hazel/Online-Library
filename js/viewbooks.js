a30eae90ee0327c0dc40722b55c26935b18a90db
Marwan06hazel <128864110+Marwan06hazel@users.noreply.github.com>
2026-04-12 10:46:50 +0200

    Update renderBooks and setupNavbar functions

--- js/viewbooks.js
+++ js/viewbooks.js
@@ -18,10 +18,19 @@ function loadLibrary() {
 
 function renderBooks(booksToDisplay) {
     booksGrid.innerHTML = "";
+    const userRole = localStorage.getItem("role");
 
     booksToDisplay.forEach(book => {
         const card = document.createElement("div");
         card.className = "book-card";
+
+        let actionButton = "";
+        if (userRole === "admin") {
+            actionButton = `<a href="managebooks.html?id=${book.id}" class="borrow-btn">Edit</a>`;
+        } else {
+            actionButton = `<a href="borrowpage.html?id=${book.id}" class="borrow-btn">Borrow</a>`;
+        }
+
         card.innerHTML = `
             <img src="${book.cover}" alt="Book Cover" class="book-cover-img">
             <div class="book-info">
@@ -30,8 +39,7 @@ function renderBooks(booksToDisplay) {
                 <p class="category">${book.Category}</p>
                 <div class="book-actions">
                     <a href="Details.html?id=${book.id}" class="details-btn">Details</a>
-                    <a href="borrowpage.html?id=${book.id}" class="borrow-btn">Borrow</a>
-                    <a href="managebooks.html?id=${book.id}" class="borrow-btn">Edit</a>
+                    ${actionButton}
                 </div>
             </div>
         `;
@@ -57,3 +65,28 @@ function setupFilters(allBooks) {
 }
 
 loadLibrary();
+
+function setupNavbar() {
+    const navbar = document.getElementById("navbar");
+    const userRole = localStorage.getItem("role");
+
+    if (userRole === "admin") {
+        // Admin Navbar
+        navbar.innerHTML = `
+            <a href="adminpage.html">Dashboard</a>
+            <a href="viewbooks.html">Manage Books</a>
+            <a href="addbook.html">Add New Book</a>
+            <a href="../index.html" class="button" onclick="logout()">Log out</a>
+        `;
+    } else {
+        // User Navbar
+        navbar.innerHTML = `
+            <a href="userpage.html">Home</a>
+            <a href="viewbooks.html">Books</a>
+            <a href="listofborrowedbooks.html">My Borrowed Books</a>
+            <a href="../index.html" class="button" onclick="logout()">Log out</a>
+        `;
+    }
+}
+
+setupNavbar();
