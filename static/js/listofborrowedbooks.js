fbb6d0451e96e0b55fee8c9b981bfb7724497d86
hendmohammad <hendmoham.25@gmail.com>
2026-05-13 01:11:57 +0300

    Update listofborrowedbooks.js

--- static/js/listofborrowedbooks.js
+++ static/js/listofborrowedbooks.js
@@ -1,29 +1,29 @@
-//--- Handling Return logic
+// //--- Handling Return logic
 
-function returnBook(bookId) {
-    fetch(`/return/${bookId}/`, {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': document.cookie.match(/csrftoken=([^;]+)/)[1]
-        }
-    }).then(() => window.location.reload());
-}
+// function returnBook(bookId) {
+//     fetch(`/return/${bookId}/`, {
+//         method: 'POST',
+//         headers: {
+//             'X-CSRFToken': document.cookie.match(/csrftoken=([^;]+)/)[1]
+//         }
+//     }).then(() => window.location.reload());
+// }
 
-//--- Handling Favourite Toggle logic
+// //--- Handling Favourite Toggle logic
 
-function favToggle(bookId, iconElement) {
-    fetch(`/favourite/${bookId}/`, {
-        method: 'POST',
-        headers: {
-            'X-CSRFToken': document.cookie.match(/csrftoken=([^;]+)/)[1]
-        }
-    }).then(res => res.json()).then(data => {
-        if (data.is_favourite) {
-            iconElement.classList.remove("fa-regular");
-            iconElement.classList.add("fa-solid");
-        } else {
-            iconElement.classList.remove("fa-solid");
-            iconElement.classList.add("fa-regular");
-        }
-    });
-}
+// function favToggle(bookId, iconElement) {
+//     fetch(`/favourite/${bookId}/`, {
+//         method: 'POST',
+//         headers: {
+//             'X-CSRFToken': document.cookie.match(/csrftoken=([^;]+)/)[1]
+//         }
+//     }).then(res => res.json()).then(data => {
+//         if (data.is_favourite) {
+//             iconElement.classList.remove("fa-regular");
+//             iconElement.classList.add("fa-solid");
+//         } else {
+//             iconElement.classList.remove("fa-solid");
+//             iconElement.classList.add("fa-regular");
+//         }
+//     });
+// }
